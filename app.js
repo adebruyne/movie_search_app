88e877bca12473ad9417fb65cb53aa2858735415
Aylin DeBruyne <adebruyne88@gmail.com>
2018-06-18 21:23:07 -0400

    connect search to results. Interpolate input into URL

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.get("/", function(req, res){
 })
 
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body ){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb" ;
+
+    request(url, function(error, response, body ){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
             res.render("results", {data: data});
